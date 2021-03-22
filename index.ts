bb920bafca588ee83c271ab9dc5dd801f1dd7571
Samual Norman <samualtnorman@gmail.com>
2021-03-22 20:42:43 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { exec } from "child_process"
+
 export function bufferToString(bufferU8View: Uint8Array) {
     const offset = 1 - (bufferU8View.length % 2)
     const u8View = new Uint8Array(bufferU8View.length + offset + 1)
@@ -11,4 +13,15 @@ export function bufferToString(bufferU8View: Uint8Array) {
 export function stringToBuffer(string: string) {
     const u8View = new Uint8Array(new Uint16Array(string.split("").map(char => char.charCodeAt(0))).buffer)
     return new Uint8Array(u8View.buffer, u8View[0] + 1)
+}
+
+export function runCommand(command: string) {
+	return new Promise<string>((resolve, reject) => {
+		exec(command, (error, stdout) => {
+			if (error)
+				return reject(error)
+
+			resolve(stdout)
+		})
+	})
 }
