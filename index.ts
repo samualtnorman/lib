85e767c577b65b3b78fb24f5ccdc1df1422a112d
Samual Norman <samualtnorman@gmail.com>
2021-09-21 18:01:57 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -76,3 +76,20 @@ export function assert(value: any, message = "assertion failed"): asserts value 
 	if (!value)
 		throw new Error(message)
 }
+
+export function tryCatch<CallbackReturn, CatchReturn>(callback: () => CallbackReturn, catchCallback: (error: unknown) => CatchReturn): CallbackReturn | CatchReturn
+export function tryCatch<CallbackReturn, ThenReturn, CatchReturn>(callback: () => CallbackReturn, thenCallback: (callbackReturn: CallbackReturn) => ThenReturn, catchCallback: (error: unknown) => CatchReturn): ThenReturn | CatchReturn
+export function tryCatch<CallbackReturn, ThenReturn, CatchReturn>(callback: () => CallbackReturn, thenOrCatchCallback: ((callbackReturn: CallbackReturn) => ThenReturn) & ((error: unknown) => CatchReturn), catchCallback?: (error: unknown) => CatchReturn) {
+	let callbackReturn
+
+	try {
+		callbackReturn = callback()
+	} catch (error) {
+		return (catchCallback || thenOrCatchCallback)(error)
+	}
+
+	if (catchCallback)
+		return thenOrCatchCallback(callbackReturn)
+
+	return callbackReturn
+}
