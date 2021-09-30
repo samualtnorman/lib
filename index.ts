d2030920200ed044a9123d51fbad62d3dd8bd7c2
Samual Norman <samualtnorman@gmail.com>
2021-09-30 17:21:44 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import { exec } from "child_process"
+import { promises as fsPromises } from "fs"
+
+const { readdir: readDirectory } = fsPromises
 
 export function bufferToString(bufferU8View: Uint8Array) {
     const offset = 1 - (bufferU8View.length % 2)
@@ -92,4 +95,98 @@ export function tryCatch<CallbackReturn, ThenReturn, CatchReturn>(callback: () =
 		return thenOrCatchCallback(callbackReturn)
 
 	return callbackReturn
+}
+
+
+
+/**
+ * @param path the directory to start recursively finding files in
+ * @param filter either a blacklist or a filter function that returns false to ignore file name
+ * @returns promise that resolves to array of found files
+ */
+export async function findFiles(path: string, filter: string[] | ((name: string) => boolean) = []) {
+	const paths: string[] = []
+
+	let filterFunction: (name: string) => boolean
+
+	if (Array.isArray(filter))
+		filterFunction = Array.prototype.includes.bind(filterFunction)
+	else
+		filterFunction = filter
+
+	for (const dirent of await readDirectory(path, { withFileTypes: true })) {
+		if (!filterFunction(dirent.name))
+			continue
+
+		const direntPath = `${path}/${dirent.name}`
+
+		if (dirent.isDirectory())
+			await findFilesSub(direntPath, filterFunction, paths)
+		else if (dirent.isFile())
+			paths.push(path)
+	}
+
+	return paths
+}
+
+export async function findFilesSub(path: string, filterFunction: (name: string) => boolean, paths: string[]) {
+	/** @type {Promise[]} */
+	const promises: Promise<any>[] = []
+
+	for (const dirent of await readDirectory(path, { withFileTypes: true })) {
+		if (!filterFunction(dirent.name))
+			continue
+
+		const direntPath = `${path}/${dirent.name}`
+
+		if (dirent.isDirectory())
+			promises.push(findFilesSub(direntPath, filterFunction, paths))
+		else if (dirent.isFile())
+			paths.push(path)
+	}
+
+	await Promise.all(promises)
+
+	return paths
+}
+
+/**
+ * for combining async generators but when you don't just want them iterated through one at a time
+ * @param asyncGenerators array of async generators
+ * @returns a singular async generator that iterates through {@link asyncGenerators} in parallel
+ */
+export async function* combineAsyncGeneratorsParallel<T>(asyncGenerators: AsyncGenerator<T, void, void>[]): AsyncGenerator<T, void, void> {
+	const promises = asyncGenerators.map(asyncGenerator => {
+		type SelfReferencingPromise = Promise<{ iteratorResult: IteratorResult<T, void>, asyncGenerator: AsyncGenerator<T, void, void>, promise: SelfReferencingPromise }>
+		const promise: SelfReferencingPromise = asyncGenerator.next().then(iteratorResult => ({ iteratorResult, asyncGenerator, promise }))
+		return promise
+	})
+
+	while (asyncGenerators.length) {
+		const { iteratorResult, asyncGenerator, promise } = await Promise.race(promises)
+
+		// type checking fails without ` == true`
+		if (iteratorResult.done == true) {
+			promises.splice(promises.indexOf(promise), 1)
+			continue
+		}
+
+		yield iteratorResult.value
+
+		const newPromise = promises[promises.indexOf(promise)] = asyncGenerator.next().then(iteratorResult => ({ iteratorResult, asyncGenerator, promise: newPromise }))
+	}
+}
+
+export async function* raceAllPromises<T>(promises: Promise<T>[]): AsyncGenerator<T, void, void> {
+	const promisesIndex = promises.map(promise => {
+		type SelfReferencingPromise = Promise<{ value: T, promise: SelfReferencingPromise }>
+		const newPromise: SelfReferencingPromise = promise.then(value => ({ value, promise: newPromise }))
+		return newPromise
+	})
+
+	while (promisesIndex.length) {
+		const { value, promise } = await Promise.race(promisesIndex)
+		yield value
+		promisesIndex.splice(promisesIndex.indexOf(promise), 1)
+	}
 }
