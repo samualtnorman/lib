20423198e0717ebb60fcaf3f27776b6633f83142
Samual Norman <samualtnorman@gmail.com>
2021-10-22 11:25:31 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -56,16 +56,16 @@ export function is<C extends Function>(object: {}, constructor: C): object is C[
 }
 
 export class DynamicMap<K, V> extends Map<K, V> {
-	constructor(private fallbackHandler: (key: K) => V) { super() }
+	constructor(
+		private fallbackHandler: (key: K) => V
+	) { super() }
 
 	override get(key: K) {
 		if (super.has(key))
 			return super.get(key)!
 
 		const value = this.fallbackHandler(key)
-
 		super.set(key, value)
-
 		return value
 	}
 }
