7bec615907e5f984bb54e43d09197ee77efc60e4
Samual Norman <samualtnorman@gmail.com>
2021-11-19 23:52:54 +0000

    rename `getHackmudCharacterCount()` to `countHackmudCharacters`

--- src/countHackmudCharacters.ts
+++ src/countHackmudCharacters.ts
@@ -1,5 +1,5 @@
-export function getHackmudCharacterCount(script: string) {
+export function countHackmudCharacters(script: string) {
 	return script.replace(/\/\/.*/g, "").replace(/[ \t\n\r\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000]/g, "").length
 }
 
-export default getHackmudCharacterCount
+export default countHackmudCharacters
