58759ba1b5531b1eae9de10148b7018cb8b19b28
Samual Norman <samualtnorman@gmail.com>
2021-12-09 19:49:33 +0000

    add `DynamicWeakMap` to `DynamicMap.ts`

--- src/DynamicMap.ts
+++ src/DynamicMap.ts
@@ -14,3 +14,18 @@ export class DynamicMap<K, V> extends Map<K, V> {
 }
 
 export default DynamicMap
+
+export class DynamicWeakMap<K extends object, V> extends WeakMap<K, V> {
+	constructor(
+		private fallbackHandler: (key: K) => V
+	) { super() }
+
+	override get(key: K) {
+		if (super.has(key))
+			return super.get(key)!
+
+		const value = this.fallbackHandler(key)
+		super.set(key, value)
+		return value
+	}
+}
