f7282b9c530edcf0e1452929bef7ad148c90f2d8
Samual Norman <samualtnorman@gmail.com>
2021-12-16 16:06:05 +0000

    simplify `findFiles()`

--- src/findFiles.ts
+++ src/findFiles.ts
@@ -7,49 +7,24 @@ const { readdir: readDirectory } = fsPromises
  * @param filter either a blacklist or a filter function that returns false to ignore file name
  * @returns promise that resolves to array of found files
  */
-export async function findFiles(path: string, filter: string[] | ((name: string) => boolean) = []) {
-	const paths: string[] = []
+export async function findFiles(path: string, filter: string[] | ((name: string) => boolean) = [], paths: string[] = []) {
+	const filterFunction = Array.isArray(filter)
+		? (name: string) => !filter.includes(name)
+		: filter
 
-	let filterFunction: (name: string) => boolean
-
-	if (Array.isArray(filter))
-		filterFunction = name => !filter.includes(name)
-	else
-		filterFunction = filter
-
-	for (const dirent of await readDirectory(path, { withFileTypes: true })) {
+	await Promise.all((await readDirectory(path, { withFileTypes: true })).map(async dirent => {
 		if (!filterFunction(dirent.name))
-			continue
+			return
 
 		const direntPath = `${path}/${dirent.name}`
 
 		if (dirent.isDirectory())
-			await findFilesSub(direntPath, filterFunction, paths)
+			await findFiles(direntPath, filterFunction, paths)
 		else if (dirent.isFile())
 			paths.push(direntPath)
-	}
+	}))
 
 	return paths
 }
 
 export default findFiles
-
-async function findFilesSub(path: string, filterFunction: (name: string) => boolean, paths: string[]) {
-	const promises: Promise<any>[] = []
-
-	for (const dirent of await readDirectory(path, { withFileTypes: true })) {
-		if (!filterFunction(dirent.name))
-			continue
-
-		const direntPath = `${path}/${dirent.name}`
-
-		if (dirent.isDirectory())
-			promises.push(findFilesSub(direntPath, filterFunction, paths))
-		else if (dirent.isFile())
-			paths.push(direntPath)
-	}
-
-	await Promise.all(promises)
-
-	return paths
-}
