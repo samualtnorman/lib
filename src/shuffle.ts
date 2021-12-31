93876f786ede46460089595f563542adfc09a2ef
Samual Norman <samualtnorman@gmail.com>
2021-12-31 21:29:18 +0000

    fix type error

--- src/shuffle.ts
+++ src/shuffle.ts
@@ -1,6 +1,6 @@
 export function shuffle<T>(array: T[]): T[] {
 	for (let index = array.length; index;)
-		array.push(array.splice(Math.floor(Math.random() * index--), 1)[0])
+		array.push(array.splice(Math.floor(Math.random() * index--), 1)[0]!)
 
 	return array
 }
