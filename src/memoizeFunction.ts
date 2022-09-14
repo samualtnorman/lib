3091cba34f682d51c51b4d9c9246fb2337eb5731
Samual Norman <samualtnorman@gmail.com>
2022-09-14 15:02:21 +0100

    add `memoizeFunction()`

---
+++ src/memoizeFunction.ts
@@ -0,0 +1,24 @@
+const defaultArgumentComparerFunction = (a: unknown[], b: unknown[]) =>
+	a.length == b.length && a.every((aItem, index) => aItem == b[index])
+
+export const memoizeFunction = <A extends unknown[], R extends unknown>(
+	function_: (...arguments_: A) => R,
+	argumentComparerFunction: (a: unknown[], b: unknown[]) => boolean = defaultArgumentComparerFunction
+) => {
+	const memos: { arguments: A, returnValue: R }[] = []
+
+	return (...arguments_: A): R => {
+		const foundMemo = memos.find(memo => argumentComparerFunction(memo.arguments, arguments_))
+
+		if (foundMemo)
+			return foundMemo.returnValue
+
+		const newMemo = { arguments: arguments_, returnValue: function_(...arguments_) }
+
+		memos.push(newMemo)
+
+		return newMemo.returnValue
+	}
+}
+
+export default memoizeFunction
