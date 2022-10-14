4330439279e3fa1f292d94696bb44a3a48db0d1a
Samual Norman <samualtnorman@gmail.com>
2022-10-14 16:26:31 +0100

    remove uneeded recursion

--- src/retry.ts
+++ src/retry.ts
@@ -1,21 +1,31 @@
+/* eslint-disable no-await-in-loop */
+
 import { LaxPartial } from "."
 import wait from "./wait"
 
 export type RetryOptions = { attempts: number, retryDelay: number, onError: (error: unknown) => void }
 
-export function retry<T>(
-	function_: () => Promise<T>,
-	{ attempts = 3, retryDelay = 1000, onError }: LaxPartial<RetryOptions> = {}
-): Promise<T> {
-	return function_().catch(async (error: unknown) => {
-		if (attempts <= 1)
-			throw error
+export async function retry<T>(
+	callback: () => Promise<T>,
 
-		onError?.(error)
-		await wait(retryDelay)
+	{
+		attempts = 3,
+		retryDelay = 1000,
+		onError = error => console.error(`Caught`, error, `retrying ${attempts} more time(s)`)
+	}: LaxPartial<RetryOptions> = {}
+) {
+	while (true) {
+		try {
+			return await callback()
+		} catch (error) {
+			if (attempts <= 1)
+				throw error
 
-		return retry(function_, { attempts: attempts - 1, retryDelay, onError })
-	})
+			attempts--
+			onError(error)
+			await wait(retryDelay)
+		}
+	}
 }
 
 export default retry
