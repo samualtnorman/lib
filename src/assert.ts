28c920ad80dee2419b65b640bb5a846da8001526
Samual Norman <samualtnorman@gmail.com>
2023-02-19 13:38:41 +0000

    make `ensure` just an alias for `assert` let `message` be a function that returns a string

--- src/assert.ts
+++ src/assert.ts
@@ -2,16 +2,13 @@ import createErrorClass from "./createErrorClass"
 
 export const AssertError = createErrorClass(`AssertError`)
 
-export function assert(value: any, message = `assertion failed`): asserts value {
+export function assert(value: any, message: (() => string) | string = `assertion failed`): asserts value {
 	if (!value)
-		throw new AssertError(message)
+		throw new AssertError(typeof message == `string` ? message : message())
+
+	return value
 }
 
 export default assert
 
-// eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-export function ensure<T>(value: T | undefined | null | void, message = `ensure failed`): T {
-	assert(value, message)
-
-	return value
-}
+export const ensure = assert as <T>(value: T | undefined | null | false | 0, message: (() => string) | string) => T
