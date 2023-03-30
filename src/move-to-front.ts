64827789873558af10ad59e7cf57af543955a43e
Samual Norman <samualtnorman@gmail.com>
2023-03-30 21:57:10 +0100

    add move-to-front encoding and decoding

---
+++ src/move-to-front.ts
@@ -0,0 +1,25 @@
+export function encode(u8View: Uint8Array): void {
+	// eslint-disable-next-line unicorn/no-useless-spread -- how is this a useless spread?
+	const list = [ ...new Array(256) ].map((_, index) => index)
+
+	for (const [ index, byte ] of u8View.entries()) {
+		const encodedByte = list.indexOf(byte)
+
+		list.splice(encodedByte, 1)
+		list.unshift(byte)
+		u8View[index] = encodedByte
+	}
+}
+
+export function decode(u8View: Uint8Array): void {
+	// eslint-disable-next-line unicorn/no-useless-spread
+	const list = [ ...new Array(256) ].map((_, index) => index)
+
+	for (const [ viewIndex, byte ] of u8View.entries()) {
+		const decodedByte = list[byte]!
+
+		list.splice(byte, 1)
+		list.unshift(decodedByte)
+		u8View[viewIndex] = decodedByte
+	}
+}
