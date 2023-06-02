70205e02b353eda4e18c5990da812bd48fcb1866
Samual Norman <samualtnorman@gmail.com>
2023-06-02 15:32:34 +0100

    add exponential backoff to `retry()`

--- src/retry.ts
+++ src/retry.ts
@@ -2,7 +2,8 @@
 import type { LaxPartial } from "."
 import { wait } from "./wait"
 
-export type RetryOptions = { attempts: number, retryDelay: number, onError: (error: unknown) => void }
+export type RetryOptions =
+	{ attempts: number, retryDelay: number, onError: (error: unknown) => void, exponentialBackoff: boolean }
 
 /** @example const response = await retry(() => fetch(url, fetchOptions)) */
 export async function retry<T>(
@@ -10,7 +11,8 @@ export async function retry<T>(
 	{
 		attempts = 3,
 		retryDelay = 1000,
-		onError = error => console.error(`Caught`, error, `retrying`, attempts, `more time(s)`)
+		onError = error => console.error(`Caught`, error, `retrying ${attempts} more time(s) in ${retryDelay}ms`),
+		exponentialBackoff = true
 	}: LaxPartial<RetryOptions> = {}
 ) {
 	while (true) {
@@ -21,6 +23,10 @@ export async function retry<T>(
 				throw error
 
 			attempts--
+
+			if (exponentialBackoff)
+				retryDelay *= 2
+
 			onError(error)
 			await wait(retryDelay)
 		}
