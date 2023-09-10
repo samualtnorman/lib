90c6a3b6c13d053109bcf504da6b1102d2f575a8
Samual Norman <me@samual.uk>
2023-09-10 15:02:41 +0100

    remove `node-fetch` dropping support for node 16

--- src/fetch/browser.ts
+++
@@ -1,3 +0,0 @@
-const fetch_ = fetch
-
-export { fetch_ as fetch, fetch_ as default }
--- src/fetch/index.ts
+++
@@ -1,3 +0,0 @@
-const fetch_ = (fetch || (await import(`node-fetch`)).default) as typeof fetch
-
-export { fetch_ as fetch, fetch_ as default }
