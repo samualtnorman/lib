dd1ea953910a501ae01e4f69950382edacd51107
Samual Norman <samualtnorman@gmail.com>
2023-09-22 16:39:22 +0100

    add `@samual/lib/cookie/typed` module

---
+++ src/cookie/typed.ts
@@ -0,0 +1,26 @@
+import * as v from "valibot"
+import * as Cookie from "."
+
+export type CookieOptions<T extends v.BaseSchema> = { name: string, schema: T }
+export const makeCookieOptions = <T extends v.BaseSchema>(options: CookieOptions<T>) => options
+
+export function getCookie<
+	T extends v.BaseSchema
+>(cookies: Map<string, string>, options: CookieOptions<T>): v.Output<T> | undefined {
+	const cookie = cookies.get(options.name)
+
+	if (cookie) {
+		const result = v.safeParse(options.schema, JSON.parse(cookie))
+
+		if (result.success)
+			return result.output
+
+		console.error(HERE, options.name, result.issues[0])
+	}
+}
+
+export const setCookie = <T extends v.BaseSchema>(options: CookieOptions<T>, value: v.Output<T>): string =>
+	Cookie.setCookie(options.name, JSON.stringify(value))
+
+export const deleteCookie = <T extends v.BaseSchema>(options: CookieOptions<T>): string =>
+	Cookie.deleteCookie(options.name)
