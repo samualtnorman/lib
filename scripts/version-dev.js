873e51b391e3c508070d705c254d1c7d100b4d05
Samual <me@samual.uk>
2024-03-06 21:50:40 +0000

    update

--- scripts/version-dev.js
+++ scripts/version-dev.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import { spawnSync } from "child_process"
 import * as semver from "semver"
-import { version } from "../package.json" assert { type: "json" }
+import packageJson from "../package.json" assert { type: "json" }
 
 const hash = spawnSync("git", [ "rev-parse", "--short", "HEAD" ], { encoding: "utf8" }).stdout.trim()
 
-spawnSync("pnpm", [ "version", `${semver.inc(version, "minor")}-${hash}` ], { stdio: "inherit" })
+spawnSync("pnpm", [ "version", `${semver.inc(packageJson.version, "minor")}-${hash}` ], { stdio: "inherit" })
 process.exit()
