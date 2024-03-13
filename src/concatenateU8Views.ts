35a6dd1cac161682d7c8bb8340ab9fb14dcac9e6
Samual <me@samual.uk>
2024-03-13 20:59:54 +0000

    fix `concatenateU8Views()`

--- src/concatenateU8Views.ts
+++ src/concatenateU8Views.ts
@@ -2,13 +2,13 @@ import { sum } from "./sum"
 
 /** @example const string = concatenateU8Views(await asyncIterableToArray(childProcess.stdout)) */
 export function concatenateU8Views(u8Views: Uint8Array[]): Uint8Array {
-	const u8View = new Uint8Array(sum(u8Views.map(({ length }) => length)))
+	const result = new Uint8Array(sum(u8Views.map(({ length }) => length)))
 	let offset = 0
 
 	for (const u8View of u8Views) {
-		u8View.set(u8View, offset)
+		result.set(u8View, offset)
 		offset += u8View.length
 	}
 
-	return u8View
+	return result
 }
