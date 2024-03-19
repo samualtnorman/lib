060013de29db45d6b3520005fefdc852c1aabb38
Samual Norman <me@samual.uk>
2024-03-19 16:00:24 +0000

    `assert()`: pass `value` into the callback

--- src/assert.ts
+++ src/assert.ts
@@ -1,16 +1,16 @@
-import type { NonFalsy } from "."
+import type { Falsy, NonFalsy } from "."
 import { createErrorClass } from "./createErrorClass"
 
 export const AssertError = createErrorClass(`AssertError`)
 
 /** @example assert(typeof maybeString == "string", () => `Got ${typeof maybeString} instead of string`) */
-export function assert(value: any, message: (() => string) | string = `assertion failed`): asserts value {
+export function assert<T>(value: T, message: ((value: T & Falsy) => string) | string = `assertion failed`): asserts value {
 	if (!value)
-		throw new AssertError(typeof message == `string` ? message : message())
+		throw new AssertError(typeof message == `string` ? message : message(value as any))
 
-	return value
+	return value as any
 }
 
 /** Like typescript's non-null assertion operator (postfix `!`), but enforced at runtime.
   * @example ensure(objectOrUndefined, "Got undefined instead of object").property */
-export const ensure = assert as <T>(value: T, message?: (() => string) | string) => NonFalsy<T>
+export const ensure = assert as <T>(value: T, message?: ((value: T & Falsy) => string) | string) => NonFalsy<T>
