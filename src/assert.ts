c45233e6a0ac5a32da3c5f4238916c75ad356bbf
Samual Norman <me@samual.uk>
2024-03-19 15:23:48 +0000

    undo simplify `assert()`'s `message` argument it can now take a string directly again

--- src/assert.ts
+++ src/assert.ts
@@ -4,13 +4,13 @@ import { createErrorClass } from "./createErrorClass"
 export const AssertError = createErrorClass(`AssertError`)
 
 /** @example assert(typeof maybeString == "string", () => `Got ${typeof maybeString} instead of string`) */
-export function assert(value: any, message = () => `assertion failed`): asserts value {
+export function assert(value: any, message: (() => string) | string = `assertion failed`): asserts value {
 	if (!value)
-		throw new AssertError(message())
+		throw new AssertError(typeof message == `string` ? message : message())
 
 	return value
 }
 
 /** Like typescript's non-null assertion operator (postfix `!`), but enforced at runtime.
   * @example ensure(objectOrUndefined, "Got undefined instead of object").property */
-export const ensure = assert as <T>(value: T, message?: () => string) => NonFalsy<T>
+export const ensure = assert as <T>(value: T, message?: (() => string) | string) => NonFalsy<T>
