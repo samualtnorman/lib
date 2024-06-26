c9591f24f1520ce88e139cb33bdbb1b853301f63
Samual Norman <me@samual.uk>
2024-06-26 11:04:26 +0100

    Update base128.ts

--- src/base128.ts
+++ src/base128.ts
@@ -1,12 +1,24 @@
-export function encode(data: { [index: number]: number, length: number }) {
+export function encode(data: { [index: number]: number, length: number }): Uint8Array {
 	const result = new Uint8Array(Math.ceil(data.length * (8 / 7)))
 
-	for (let index = data.length; index--;) {
-		const resultIndex = Math.floor(index * (8 / 7))
+	for (let index = result.length; index--;) {
+		const offset = (index % 8) + 1
+
+		result[index] = (((data[Math.floor(index * (7 / 8))]!) << (8 - offset)) & 0x7F) +
+			((data[Math.floor((index + 1) * (7 / 8))] || 0) >> offset)
+	}
+
+	return result
+}
+
+export function decode(data: { [index: number]: number, length: number }): Uint8Array {
+	const result = new Uint8Array(Math.floor(data.length * (7 / 8)))
+
+	for (let index = result.length; index--;) {
+		const leftIndex = Math.floor(index * (8 / 7))
 		const offset = (index % 7) + 1
 
-		result[resultIndex] |= data[index]! >> offset
-		result[resultIndex + 1] |= ((data[index]! & ((1 << offset) - 1)) << 7) >> offset
+		result[index] = ((data[leftIndex]!) << offset) + ((data[leftIndex + 1] || 0) >> (7 - offset))
 	}
 
 	return result
