4cfa9fa6f1264015990a6c6f6b84c78136ab2b73
Samual Norman <me@samual.uk>
2024-08-02 16:05:20 +0100

    drop support for node versions below 18.20 and 20.10 but gain support for building with 22

--- scripts/version-dev.js
+++ scripts/version-dev.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { spawnSync } from "child_process"
 import * as semver from "semver"
-import packageJson from "../package.json" assert { type: "json" }
+import packageJson from "../package.json" with { type: "json" }
 
 const hash = spawnSync("git", [ "rev-parse", "--short", "HEAD" ], { encoding: "utf8" }).stdout.trim()
 
